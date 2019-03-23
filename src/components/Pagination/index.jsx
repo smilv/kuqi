a9a38d407a90be834b8cc5de1c5364e0809d08bb
smilv <owou@icloud.com>
2019-03-23 16:59:01 +0800

    add avatar

--- src/components/Pagination/index.jsx
+++ src/components/Pagination/index.jsx
@@ -1,5 +1,9 @@
 import React, { Component } from "react";
 
-class Pagination extends Component {}
+class Pagination extends Component {
+    render() {
+        return <div>分页</div>;
+    }
+}
 
 export default Pagination;
