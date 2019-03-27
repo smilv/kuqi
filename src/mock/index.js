b92f837a42419a8e2cf48279b97134a7b7c2b54a
smilv <owou@icloud.com>
2019-03-27 18:49:18 +0800

    mock

--- src/mock/index.js
+++ src/mock/index.js
@@ -2,18 +2,17 @@ import Mock from "mockjs";
 
 //指定被拦截的 Ajax 请求的响应时间
 Mock.setup({
-    timeout: "200-1000"
+    timeout: "10-1000"
 });
 
 Mock.mock(/user\/info/, "post", {
-    code: 0,
+    code: 200,
     data: {
+        // 属性 list 的值是一个数组，其中含有 1 到 10 个元素
         "list|1-10": [
             {
                 // 属性 id 是一个自增数，起始值为 1，每次增 1
-                "id|+1": 1,
-                title: "前端人人@id",
-                status: 1
+                "id|+1": 1
             }
         ]
     },
